3db3d3969fd74acbae9e521a18dbeb7268641c62
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-12-02 16:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const btn=document.getElementById("btn");
+const output=document.getElementById("output")
+
+btn.addEventListener('click',display);
+
+async function display(){
+	const time=document.getElementById("delay").value*1000
+	const text=document.getElementById("text").value
+	setTimeout(()=>{
+		output.innerText=text;
+	},time)
+}
